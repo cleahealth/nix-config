8359981b4d20542a9f41aa3ba31ef8d717970295
Tommy Li <dev@stdx.space>
2025-03-14 13:39:20 +0800

    add office tools to devops desktop

--- profiles/nixos/devops-desktop/default.nix
+++ profiles/nixos/devops-desktop/default.nix
@@ -6,6 +6,7 @@
     ../../../modules/nixos/base-desktop
     ../../../modules/nixos/dev-tools
     ../../../modules/nixos/dev-tools-desktop
+    ../../../modules/nixos/office-tools
     ../../../modules/nixos/ops-tools
   ];
 }
